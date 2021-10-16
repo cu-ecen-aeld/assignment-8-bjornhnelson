7695b69cb73ca62f8a5bac81dc72286deb7e0b21
Bjorn Nelson <bjornhnelson@gmail.com>
2021-10-16 17:33:45 -0600

    ldd package configuration

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,20 @@
+# LDD
+
+AESD_ASSIGNMENTS_VERSION = 05014e83df3205b72b1626f928bd53636c38c53c
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignment-7-bjornhnelson.git
+AESD_ASSIGNMENTS_SITE_METHOD = git
+AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += scull
+LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+
+define LDD_INSTALL_TARGET_CMDS
+    $(INSTALL) -m 0700 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0700 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0700 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0700 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
